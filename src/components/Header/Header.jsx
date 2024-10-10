ee04d3c88254bba7cf49133ed4347b85ddd0d2a6
CroWzblooD <91479132+CroWzblooD@users.noreply.github.com>
2024-10-10 18:54:04 +0530

    new

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ import star from './../../assets/star.png';
 import user1 from './../../assets/user1.png';
 import user2 from './../../assets/user2.png';
 import user3 from './../../assets/user3.png';
-import headerIMG from './../../assets/header-img.jpg';
+import headerIMG from './../../assets/header-img.png';
 import arrow from './../../assets/arrow.png';
 
 function Header() {
